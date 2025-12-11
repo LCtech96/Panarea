32a81c28c48ae9938de203ae2ed12224d18cc7da
LCtech96 <lucacorrao96@outlook.it>
2025-12-11 23:35:43 +0100

    Add ServiceWorkerHandler to remove service worker registration errors

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,12 +2,14 @@
 
 import { OrderProvider } from '@/contexts/OrderContext'
 import { ThemeProvider } from '@/contexts/ThemeContext'
+import ServiceWorkerHandler from '@/components/ServiceWorkerHandler'
 import { ReactNode } from 'react'
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider>
       <OrderProvider>
+        <ServiceWorkerHandler />
         {children}
       </OrderProvider>
     </ThemeProvider>
---
+++ components/ServiceWorkerHandler.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function ServiceWorkerHandler() {
+  useEffect(() => {
+    // Rimuovi eventuali service worker registrati in precedenza
+    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then((registrations) => {
+        for (const registration of registrations) {
+          registration.unregister().catch((error) => {
+            console.log('Service worker unregistration error:', error)
+          })
+        }
+      })
+
+      // Gestisci eventuali errori di preload
+      navigator.serviceWorker.addEventListener('message', (event) => {
+        if (event.data && event.data.type === 'SKIP_WAITING') {
+          // Ignora messaggi di skip waiting
+        }
+      })
+    }
+  }, [])
+
+  return null
+}
+
